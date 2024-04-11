92af54ff162ebc7042f50951f12c705e5e8ce3af
Marco Sérvio <marcoservio22@hotmail.com>
2024-04-11 20:25:23 -0300

    feat: adicionando exceptions e mensagens de exceptions para registrar usuario.

---
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,5 @@
+﻿namespace MyRecipeBook.Exceptions.ExceptionsBase;
+public class ErrorOnValidationException(IList<string> errorMessages) : MyRecipeBookException
+{
+    public IList<string> ErrorMessages { get; set; } = errorMessages;
+}
