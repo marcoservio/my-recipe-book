50df0f149c34a4c50819f25272f3bd871df578bb
Marco Sérvio <marcoservio22@hotmail.com>
2024-04-12 10:17:59 -0300

    feat: adicionando web-api com endpoint registrar usuario.

---
+++ src/Backend/MyRecipeBook.API/Middleware/CultureMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace MyRecipeBook.API.Middleware;
+
+public class CultureMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task Invoke(HttpContext context)
+    {
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureInfo = new CultureInfo("en");
+
+        if (!string.IsNullOrWhiteSpace(requestedCulture) && supportedLanguages.Any(c => c.Name.Equals(requestedCulture)))
+            cultureInfo = new CultureInfo(requestedCulture);
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+    }
+}
