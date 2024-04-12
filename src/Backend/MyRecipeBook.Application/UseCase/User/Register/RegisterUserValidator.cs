237ac53545dbdecd28c5627251e1f083dd115705
Marco Sérvio <marcoservio22@hotmail.com>
2024-04-12 18:58:00 -0300

    feat: criação de teste de unidades para o validator de user.

--- src/Backend/MyRecipeBook.Application/UseCase/User/Register/RegisterUserValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCase/User/Register/RegisterUserValidator.cs
@@ -10,7 +10,10 @@ public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
     {
         RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
         RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.EMAIL_EMPTY);
-        RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
-        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_CHARACTERES);
+        RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_INVALID);
+        When(user => !string.IsNullOrWhiteSpace(user.Email), () =>
+        {
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+        });
     }
 }
