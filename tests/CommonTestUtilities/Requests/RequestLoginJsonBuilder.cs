a0457dbd1367cddb2a2baed979933313ce1afcd6
Marco Sérvio <marcoservio22@hotmail.com>
2024-04-15 18:04:52 -0300

    feat: testes de unidade do usecase e do validator para login.

---
+++ tests/CommonTestUtilities/Requests/RequestLoginJsonBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestLoginJsonBuilder
+{
+    public static RequestLoginJson Build(int passwordLength = 10)
+    {
+        return new Faker<RequestLoginJson>()
+            .RuleFor(l => l.Email, (f) => f.Internet.Email())
+            .RuleFor(l => l.Password, (f) => f.Internet.Password(passwordLength));
+    }
+}
--- tests/Validators.Test/Login/DoLogin/DoLoginValidatorTest.cs
+++ tests/Validators.Test/Login/DoLogin/DoLoginValidatorTest.cs
@@ -2,48 +2,32 @@
 
 using FluentAssertions;
 
+using MyRecipeBook.Application.UseCase.Login.DoLogin;
 using MyRecipeBook.Application.UseCase.User.Register;
 using MyRecipeBook.Exceptions;
 
 using Xunit;
 
-namespace Validators.Test.User.Register;
+namespace Validators.Test.Login.DoLogin;
 
-public class RegisterUserValidatorTest
+public class DoLoginValidatorTest
 {
     [Fact]
     public void Success()
     {
-        var validator = new RegisterUserValidator();
-
-        var request = RequestRegisterUserJsonBuilder.Build();
-
+        var validator = new DoLoginValidator();
+        var request = RequestLoginJsonBuilder.Build();
+        
         var result = validator.Validate(request);
 
         result.IsValid.Should().BeTrue();
     }
 
     [Fact]
-    public void Error_Name_Empty()
-    {
-        var validator = new RegisterUserValidator();
-
-        var request = RequestRegisterUserJsonBuilder.Build();
-        request.Name = string.Empty;
-
-        var result = validator.Validate(request);
-
-        result.IsValid.Should().BeFalse();
-        result.Errors.Should().ContainSingle()
-            .And.Contain(e => e.ErrorMessage.Equals(ResourceMessagesException.NAME_EMPTY));
-    }
-
-    [Fact]
     public void Error_Email_Empty()
     {
-        var validator = new RegisterUserValidator();
-
-        var request = RequestRegisterUserJsonBuilder.Build();
+        var validator = new DoLoginValidator();
+        var request = RequestLoginJsonBuilder.Build();
         request.Email = string.Empty;
 
         var result = validator.Validate(request);
@@ -56,9 +40,8 @@ public class RegisterUserValidatorTest
     [Fact]
     public void Error_Email_Invalid()
     {
-        var validator = new RegisterUserValidator();
-
-        var request = RequestRegisterUserJsonBuilder.Build();
+        var validator = new DoLoginValidator();
+        var request = RequestLoginJsonBuilder.Build();
         request.Email = "emailinvalid";
 
         var result = validator.Validate(request);
@@ -77,9 +60,8 @@ public class RegisterUserValidatorTest
     [InlineData(5)]
     public void Error_Password_Invalid(int passwordLength)
     {
-        var validator = new RegisterUserValidator();
-
-        var request = RequestRegisterUserJsonBuilder.Build(passwordLength);
+        var validator = new DoLoginValidator();
+        var request = RequestLoginJsonBuilder.Build(passwordLength);
 
         var result = validator.Validate(request);
 
