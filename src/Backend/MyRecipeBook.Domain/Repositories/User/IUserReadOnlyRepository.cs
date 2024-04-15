0fc71a81586dae9c4c05ed9447eb0aa89bcdf4e2
Marco Sérvio <marcoservio22@hotmail.com>
2024-04-15 10:57:53 -0300

    feat: criação do repositorio para recuperar um usuairo por email e senha.

--- src/Backend/MyRecipeBook.Domain/Repositories/User/IUserReadOnlyRepository.cs
+++ src/Backend/MyRecipeBook.Domain/Repositories/User/IUserReadOnlyRepository.cs
@@ -2,4 +2,5 @@
 public interface IUserReadOnlyRepository
 {
     Task<bool> ExistActiveUserWithEmail(string email);
+    Task<Entities.User?> GetByEmailAndPassword(string email, string password);
 }
