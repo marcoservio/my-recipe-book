de16aa149eab60827476d7473a67ae573f4f5cdf
Marco Sérvio <marcoservio22@hotmail.com>
2024-04-17 17:35:44 -0300

    refactor: refatoração dos teste de integração.

--- tests/WebApi.Test/Login/DoLogin/DoLoginTest.cs
+++ tests/WebApi.Test/Login/DoLogin/DoLoginTest.cs
@@ -1,37 +1,37 @@
-﻿using MyRecipeBook.Communication.Requests;
+﻿using CommonTestUtilities.Requests;
 
-using System.Net.Http.Json;
+using FluentAssertions;
+
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+
+using System.Globalization;
 using System.Net;
 using System.Text.Json;
 
 using WebApi.Test.InlineData;
 
 using Xunit;
-using FluentAssertions;
-using CommonTestUtilities.Requests;
-using MyRecipeBook.Exceptions;
-using System.Globalization;
 
 namespace WebApi.Test.Login.DoLogin;
 
-public class DoLoginTest(CustomWebApplicationFactory factory) : IClassFixture<CustomWebApplicationFactory>
+public class DoLoginTest(CustomWebApplicationFactory factory) : MyRecipeBookClassFixture(factory)
 {
     private readonly string _method = "login";
-    private readonly HttpClient _httpClient = factory.CreateClient();
-    private readonly string _email = factory.GetEmail();
-    private readonly string _password = factory.GetPassword();
-    private readonly string _name = factory.GetName();
+    private readonly Lazy<string> _email = new(factory.GetEmail);
+    private readonly Lazy<string> _password = new(factory.GetPassword);
+    private readonly Lazy<string> _name = new(factory.GetName);
 
     [Fact]
     public async Task Success()
     {
         var request = new RequestLoginJson
         {
-            Email = _email,
-            Password = _password
+            Email = _email.Value,
+            Password = _password.Value
         };
 
-        var response = await _httpClient.PostAsJsonAsync(_method, request);
+        var response = await DoPost(_method, request);
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
@@ -39,7 +39,7 @@ public class DoLoginTest(CustomWebApplicationFactory factory) : IClassFixture<Cu
         var responseData = await JsonDocument.ParseAsync(responseBody);
 
         responseData.RootElement.GetProperty("name").GetString()
-            .Should().NotBeNullOrWhiteSpace().And.Be(_name);
+            .Should().NotBeNullOrWhiteSpace().And.Be(_name.Value);
     }
 
     [Theory]
@@ -48,12 +48,7 @@ public class DoLoginTest(CustomWebApplicationFactory factory) : IClassFixture<Cu
     {
         var request = RequestLoginJsonBuilder.Build();
 
-        if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
-            _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
-
-        _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
-
-        var response = await _httpClient.PostAsJsonAsync(_method, request);
+        var response = await DoPost(_method, request, culture);
 
         response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
 
---
+++ tests/WebApi.Test/MyRecipeBookClassFixture.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+
+using Xunit;
+
+namespace WebApi.Test;
+
+public class MyRecipeBookClassFixture(CustomWebApplicationFactory factory) : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _httpClient = factory.CreateClient();
+
+    protected async Task<HttpResponseMessage> DoPost(string method, object request, string culture = "en")
+    {
+        ChangeRequestCulture(culture);
+
+        return await _httpClient.PostAsJsonAsync(method, request);
+    }
+
+    private void ChangeRequestCulture(string culture)
+    {
+        if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
+            _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
+
+        _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+    }
+}
