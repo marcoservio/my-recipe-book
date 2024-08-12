e2dbf76113ab9d599580130f0c25d731cf54d0d8
Marco Sérvio <marcoservio22@hotmail.com>
2024-08-12 18:01:30 -0300

    feat: recuperar uma receita com uma imagem associada.

--- src/Backend/MyRecipeBook.Infrastructure/Services/Storage/AzureStorageService.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Services/Storage/AzureStorageService.cs
@@ -1,13 +1,45 @@
 ﻿using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
 
 using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Extensions;
 using MyRecipeBook.Domain.Services.Storage;
+using MyRecipeBook.Domain.ValueObjects;
 
 namespace MyRecipeBook.Infrastructure.Services.Storage;
 
 public class AzureStorageService(BlobServiceClient blobServiceClient) : IBlobStorageService
 {
     private readonly BlobServiceClient _blobServiceClient = blobServiceClient;
+
+    public async Task<string> GetImageUrl(User user, string fileName)
+    {
+        var containerName = user.UserIdentifier.ToString();
+
+        var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        var exist = await containerClient.ExistsAsync();
+        if (exist.Value.IsFalse())
+            return string.Empty;
+
+        var blobClient = containerClient.GetBlobClient(fileName);
+        exist = await blobClient.ExistsAsync();
+        if (exist.Value)
+        {
+            var sasBuilder = new BlobSasBuilder
+            {
+                BlobContainerName = containerName,
+                BlobName = fileName,
+                Resource = "b",
+                ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(MyRecipeBookRuleConstants.MAXIMUM_IMAGE_URL_LIFETIME_IN_MINUTES),
+            };
+
+            sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+            return blobClient.GenerateSasUri(sasBuilder).ToString();
+        }
+
+        return string.Empty;
+    }
 
     public async Task Upload(User user, Stream file, string fileName)
     {
